e63af5972f922ad6a3d10b78d7668b0cd1a0c309
Amanda Casten <ascasten@gmail.com>
2020-06-29 23:04:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,6 @@ var chocolateBars =["snickers","hundred grand","kit kat","skittles"];
 function addElementToBeginningOfArray(newData) {
   var newChocolateBars =[newData, ...chocolateBars]
   return newChocolateBars;
-  
 }
 
 function destructivelyAddElementToBeginningOfArray(newData) {
